9b55f41d7223528df38aac2c95cec378f83f268d
Kavindra Singh Bisht <kavin@tripoto.com>
2017-06-15 18:31:22 +0530

    chromedriverpathchange

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -17,8 +17,7 @@ public class NewTest {
 //				+ File.separator + "Shared" + File.separator
 //				+ "Jenkins" + File.separator + "Home" + File.separator
 //				+ "workspace" + File.separator + "ankit"+ File.separator + "Driver"+ File.separator + "chromedriver");
-		System.setProperty("webdriver.chrome.driver","https://staging.tripoto.com/jenkins/job/ImageServerError/ws/Driver/chromedriver");
-		
+		System.setProperty("webdriver.chrome.driver","/home/testing/Downloads/chromedriver");
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		// check for server image
