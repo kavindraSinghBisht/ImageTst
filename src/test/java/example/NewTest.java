8584681d4a57a7acab6b4a404df8ed519ecca40e
Kavindra Singh Bisht <kavin@tripoto.com>
2017-06-19 11:54:09 +0530

    url change

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -15,23 +15,22 @@ public class NewTest {
 	public static void main() throws InterruptedException {
 		System.out.println("Start");
 
-//		 System.setProperty("webdriver.chrome.driver", File.separator +
-//		 "Users"
-//		 + File.separator + "Shared" + File.separator
-//		 + "Jenkins" + File.separator + "Home" + File.separator
-//		 + "workspace" + File.separator + "ankit"+ File.separator + "driver"+
-//		 File.separator + "chromedriver");
+		 System.setProperty("webdriver.chrome.driver", File.separator +
+		 "Users"
+		 + File.separator + "Shared" + File.separator		 + "Jenkins" + File.separator + "Home" + File.separator
+		 + "workspace" + File.separator + "ankit"+ File.separator + "driver"+
+		 File.separator + "chromedriver");
 
-		System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/workspace/ImageTestServer1/Driver/chromedriver_64");
-	    DesiredCapabilities capabilities=DesiredCapabilities.chrome();
-	    capabilities.setCapability("marionette", true);
+//		System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/workspace/ImageTestServer1/Driver/chromedriver_64");
+//	    DesiredCapabilities capabilities=DesiredCapabilities.chrome();
+//	    capabilities.setCapability("marionette", true);
 
 //		ChromeDriverManager.getInstance().setup();
 		 
 		ChromeDriver driver = new ChromeDriver();
 		// check for server image
 		Thread.sleep(4000L);
-		driver.get("https://image.tripotoo.com/image/generate/ss/img/403189/TripDocument/1497424269_img_8.jpg");
+		driver.get("https://image.tripoto.com/image/generate/ss/img/403189/TripDocument/1497424269_img_8.jpg");
 		Thread.sleep(4000L);
 		LogEntries logs = driver.manage().logs().get("browser");
 		if (logs.getAll().size() > 0) {
@@ -42,7 +41,7 @@ public class NewTest {
 			// ======error found===
 			// check for original image
 
-			driver.get("https://stagingimage.tripotoo.com/media/transfer/img/30914/TripDocument/1497341053_dsc_0075.jpg");
+			driver.get("https://image.tripoto.com/media/transfer/img/403189/TripDocument/1497424269_img_8.jpg");
 			Thread.sleep(4000L);
 			LogEntries logs1 = driver.manage().logs().get("browser");
 			for (LogEntry entry : logs1) {
